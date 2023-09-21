586d7b64e81d100d8e83a37da0f3130796c5672a
tiennt <tokentien@gmail.com>
2023-09-21 20:10:39 +0700

    fix: load module

--- src/Providers/LaravelModulesCommandServiceProvider.php
+++ src/Providers/LaravelModulesCommandServiceProvider.php
@@ -15,22 +15,9 @@ class LaravelModulesCommandServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../../config/laravel-modules-command.php', 'laravel-modules-command');
-        Helper::overwrireModulesConfig();
 
-        $this->app->singleton(RepositoryInterface::class, function ($app) {
-            $path = $app['config']->get('modules.paths.modules');
-
-            return new LaravelFileRepository($app, $path);
-        });
-
-        $this->app->singleton(MigrationCreator::class, function ($app) {
-            return new \SocolaDaiCa\LaravelModulesCommand\Overwrite\MigrationCreator(
-                $app['files'],
-                $app->basePath('stubs')
-            );
-        });
-
-        $this->app->register(\Nwidart\Modules\LaravelModulesServiceProvider::class);
+        // $this->app->register(\Nwidart\Modules\LaravelModulesServiceProvider::class);
+        $this->app->register(OverwriteLaravelModulesServiceProvider::class);
     }
 
     /**
@@ -42,13 +29,6 @@ class LaravelModulesCommandServiceProvider extends ServiceProvider
             $this->app->singleton(ConsoleKernel::class);
             $this->app->make(ConsoleKernel::class);
         }
-
-        $this->app->when(MigrationCreator::class)
-            ->needs('$customStubPath')
-            ->give(function ($app) {
-                return $app->basePath('stubs');
-            })
-        ;
 
         $this->app->singleton(OpenPhpstorm::class);
     }
---
+++ src/Providers/OverwriteLaravelModulesServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SocolaDaiCa\LaravelModulesCommand\Providers;
+
+use Illuminate\Database\Migrations\MigrationCreator;
+use Illuminate\Support\ServiceProvider;
+use Nwidart\Modules\Contracts\RepositoryInterface;
+use Nwidart\Modules\LaravelModulesServiceProvider;
+use SocolaDaiCa\LaravelModulesCommand\Console\Kernel as ConsoleKernel;
+use SocolaDaiCa\LaravelModulesCommand\Helper;
+use SocolaDaiCa\LaravelModulesCommand\OpenPhpstorm;
+use SocolaDaiCa\LaravelModulesCommand\Overwrite\LaravelFileRepository;
+
+class OverwriteLaravelModulesServiceProvider extends LaravelModulesServiceProvider
+{
+    public function register()
+    {
+        parent::register();
+        Helper::overwrireModulesConfig();
+
+        $this->app->singleton(RepositoryInterface::class, function ($app) {
+            $path = $app['config']->get('modules.paths.modules');
+
+            return new LaravelFileRepository($app, $path);
+        });
+
+        $this->app->singleton(MigrationCreator::class, function ($app) {
+            return new \SocolaDaiCa\LaravelModulesCommand\Overwrite\MigrationCreator(
+                $app['files'],
+                $app->basePath('stubs')
+            );
+        });
+    }
+
+    /**
+     * Boot the application events.
+     */
+    public function boot()
+    {
+        parent::boot();
+        $this->app->when(MigrationCreator::class)
+            ->needs('$customStubPath')
+            ->give(function ($app) {
+                return $app->basePath('stubs');
+            })
+        ;
+    }
+}
