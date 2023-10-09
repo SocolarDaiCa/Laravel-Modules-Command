2e3732c65d70463539f2293d19f23c64f6dbba52
Nguyen The Tien <tokentien@gmail.com>
2023-10-09 14:59:50 +0700

    feat: command

--- src/Console/Commands/ConsoleMakeCommand.php
+++ src/Console/Commands/ConsoleMakeCommand.php
@@ -7,4 +7,15 @@ use SocolaDaiCa\LaravelModulesCommand\Console\GeneratorCommand;
 class ConsoleMakeCommand extends \Illuminate\Foundation\Console\ConsoleMakeCommand
 {
     use GeneratorCommand;
+
+    protected function buildClass($name)
+    {
+        $code = parent::buildClass($name);
+
+        $replaces = [
+            'use Illuminate\Console\Command;' => 'use SocolaDaiCa\LaravelBadassium\Contracts\Console\Command;',
+        ];
+
+        return str_replace(array_keys($replaces), array_values($replaces), $code);
+    }
 }
